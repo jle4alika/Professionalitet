5be72b1b7dcd3040b9943613083f579d95195312
jle4alika <alex.naraev@yandex.ru>
2026-03-17 11:31:04 +0700

    added CORS middleware

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,21 +1,28 @@
 import uvicorn
 from fastapi import FastAPI
 from backend.api.routers import main_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="API для сервиса аренды вендинговых аппаратов",
 )
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(main_router)
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
 
 
 if __name__ == '__main__':
